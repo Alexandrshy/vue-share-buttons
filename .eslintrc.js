55120268c754e98e0d44f0e0f4188992109dddd0
NeonGC <admin@neongc.ru>
2022-01-30 15:02:31 +0300

    update dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: ["Icon"],
+      },
+    ],
+  },
+};
